5c61dcfa9f3d01b9c570fef6113ad699e94cdc98
Alexis Koffi <saint.dadier@gmail.com>
2022-01-21 11:34:36 +0000

    Finish custom template tags

--- blango/urls.py
+++ blango/urls.py
@@ -21,4 +21,5 @@ from blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('<slug:slug>', views.post_detail, name='blog-post-detail')
 ]
