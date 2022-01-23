85c5eb6444432b0d25134a90d3c0c138036a1581
Alexis Koffi <saint.dadier@gmail.com>
2022-01-23 13:19:08 +0000

    Finish django registration

--- blango/urls.py
+++ blango/urls.py
@@ -18,13 +18,28 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from django_registration.backends.activation.views import RegistrationView
+
 from blog import views
+from blango_auth import views as auth
+from blango_auth.forms import BlangoRegistrationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('<slug:slug>', views.post_detail, name='blog-post-detail'),
     path("ip/", views.get_ip),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", auth.profile, name="profile"),
+
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
+    path("accounts/", include("django_registration.backends.activation.urls")),
+
 ]
 
 if settings.DEBUG:
