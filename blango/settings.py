560cb6326b1443057923be6cd03875ab9e2f2d9e
Alexis Koffi <saint.dadier@gmail.com>
2022-01-25 22:28:54 +0000

    Finish permissions

--- blango/settings.py
+++ blango/settings.py
@@ -219,11 +219,14 @@ class Dev(Configuration):
     ACCOUNT_AUTHENTICATION_METHOD = "email"
 
     REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
     }
 class Prod(Dev):
     DEBUG = False
